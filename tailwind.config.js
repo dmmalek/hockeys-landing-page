ed80da54bbe4231cdb529cc4840e9fe3f5a86ebe
Abdul Malek <abdulmalekmonir97@gmail.com>
2025-06-06 13:14:13 +0600

    designed nav section and responsive for mobile

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        color: {
+            'metal': '#565584',
+            'purple': '#3f3cbb',
+        },
+        extend: {},
+    },
+    plugins: [],
+}
